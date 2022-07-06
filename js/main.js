d8d43a5251f35743da7f0d5b77c9e62caa1f5f9a
jclane <justin@justinlane.me>
2022-07-06 15:54:29 -0400

    finally got `fetch` working

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-const ProductObj = (pid, pname, shortDesc) => {
-    this.pid = pid;
-    this.pname = pname;
-    this.shortDesc = shortDesc;
-    this.longDesc = "";
+const createImgElement = (src, altTxt) => {
+    let img = document.createElement("img");
+    img.setAttribute("src", src);
+    img.setAttribute("alt", altTxt);
+    return img;
+}
+
+const buildListItem = (itemData) => {
+    let li = document.createElement("li");
+    let img = createImgElement(itemData["img"], itemData["imgAltText"]);
+
+    let name = document.createElement("a");
+    name.setAttribute("class", "product-name");
+    name.setAttribute("href", "#");
+
+    let shortDesc = document.createElement("span");
+    shortDesc.setAttribute("class", "product-desc")
+    shortDesc.innerText = itemData.shortDesc;
+
+    console.log(li, img, name, shortDesc);
 }
 
 const getData = () => {
-    return fetch("./db/filler.json")
-        .then(response => {
-            return response.json();
-        }).then(data => console.log(data));
+    let data = [];
+    fetch(".\\db\\filler.json")
+        .then(res => {return res.json()})
+        .then(json => {
+            for (let i=0;i<json.length;i++) {
+                data.push(json[i]);
+            }
+        })
+        .catch(err => console.log(err));
+
+    return data;
 }
 
 const DATA = getData();
-
-for (let i = 0; i < getData.length; i++) {
-    console.log(DATA[i]);
+const TRENDING = document.querySelector("#trending-products");
+console.log(DATA);
+for (let i = 0; i < DATA.length; i++) {
+    console.log(i);
 }
